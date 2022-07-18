d29a389adf2ea39e6d4ded356588c603cb68d587
andywtse <atse4995@email.com>
2022-07-18 10:47:22 -0400

    Adds axios calls and more routes

--- controllers/weatherPrefs.js
+++ controllers/weatherPrefs.js
@@ -1,16 +1,48 @@
 import { WeatherPref } from '../models/weatherPref.js'
+import axios from 'axios'
 
-function getCurrenct(req, res) {
+const BASE_URL = `http://api.weatherapi.com/v1`
+
+function getCurrent(req, res) {
+  axios.get(`${BASE_URL}/current.json?key=${process.env.WEATHER_APIKEY}&q=${req.params.location}`)
+  .then(apiResponse => {
+    res.json(apiResponse.data)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
 }
 
 function getHourly(req, res) {
+  axios.get(`${BASE_URL}/forecast.json?key=${process.env.WEATHER_APIKEY}&q=${req.params.location}&days=1&aqi=no&alerts=no`)
+  .then(apiResponse => {
+    res.json(apiResponse.data)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
 }
 
 function getDaily(req, res) {
+  axios.get(`${BASE_URL}/forecast.json?key=${process.env.WEATHER_APIKEY}&q=${req.params.location}&days=3&aqi=no&alerts=no`)
+  .then(apiResponse => {
+    res.json(apiResponse.data)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
+}
+
+function getWeatherPref(req,res) {
+
 }
 
 export {
   getCurrent,
   getHourly,
-  getDaily
+  getDaily,
+  getWeatherPref
 }
--- routes/weatherPrefs.js
+++ routes/weatherPrefs.js
@@ -1,13 +1,18 @@
 import { Router } from 'express'
 import * as weatherController from '../controllers/weatherPrefs.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 
 const router = Router()
 
 /*---------- Public Routes ----------*/
+
+
 router.get('/:location/current', weatherController.getCurrent)
 router.get('/:location/hourly', weatherController.getHourly)
 router.get('/:location/daily', weatherController.getDaily)
 
 /*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+router.get('/preference', weatherController.getWeatherPref)
 
 export { router }

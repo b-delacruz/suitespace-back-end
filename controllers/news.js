94722ed5164d65dae0b3ddfd4ceedf8be49798fc
b-delacruz <brianmdelacruz@gmail.com>
2022-07-18 09:45:28 -0400

    add API response

--- controllers/news.js
+++ controllers/news.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 function getNews(req, res) {
-  axios.get (`https://https://content.guardianapis.com/search?q=${req.params.search}&api-key=${process.env.NEWS_APIKEY}`)
+  axios.get (`https://content.guardianapis.com/search?q=${req.params.search}&api-key=${process.env.NEWS_APIKEY}`)
   .then(apiResponse => {
     res.json(apiResponse.data)
   })

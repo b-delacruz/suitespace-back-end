10baf96706df61f8d4976419728eb03258d79b9f
mauLovera <mauricioloveraedu@gmail.com>
2022-07-19 12:43:41 -0400

    update calendar event model changes

--- controllers/calendars.js
+++ controllers/calendars.js
@@ -2,11 +2,25 @@ import { Profile } from '../models/profile.js'
 import { CalendarEvent } from '../models/calendarEvent.js'
 
 function index(req, res) {
-
+  CalendarEvent.find({})
+  .then(event => {
+    res.json(event)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
 }
 
 function create(req, res) {
-
+  CalendarEvent.create(req.body)
+  .then(event => {
+    res.json(event)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
 }
 
 function deleteEvent(req, res) {
